7459d2309ba88c8e6d202cad5e191ba8a6a24a9d
abnerAmos <abner.fsouza1@gmail.com>
2023-06-14 08:39:04 -0300

    Teste JUnit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ServiceApplicationTests {
+
+	// Captura a porta gerada em uma variável.
+	@LocalServerPort
+	private int port;
 
 	@Test
-	void contextLoads() {
+	void mustReturnStatusOk_WhenConsultKitchen() {
+		RestAssured
+			.given()
+				.basePath("/test/list")
+				.port(port)
+				.accept(ContentType.JSON)
+			.when()
+				.get()
+			.then()
+				.statusCode(HttpStatus.OK.value());
+
 	}
 
 }
